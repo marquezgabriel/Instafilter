73b94a33fe471def1ec3d7e4439499f893c13a5e
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-15 15:04:28 -0400

    Ready for day 65

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,37 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-               showingImagePicker = true
-            }
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage()}
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        
-      
+        Text("Hello, world!")
+            .padding()
     }
 }
 
